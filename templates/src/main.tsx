f7409ff9c95e1b3b2346f5b9bbd47047b9d14542
giovandi <giovandi@marcopolo.com.br>
2025-09-29 20:49:03 -0300

    feat: update index.html

--- templates/src/main.tsx
+++ templates/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { widgetStart, require3Dx, disableDefaultCSS } from 'widget-3dx'
+import { widgetStart, require3Dx, disableDefaultCSS } from 'widget-3dx-react'
 
 widgetStart(() => {
   disableDefaultCSS();
